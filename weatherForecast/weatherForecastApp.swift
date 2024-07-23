c49058492c6572c1b5feaded2bd98e8bac2d1a74
VladMin10 <miniailowladislav@gmail.com>
2024-07-23 14:50:34 +0300

    Initial Commit

---
+++ weatherForecast/weatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherForecastApp.swift
+//  weatherForecast
+//
+//  Created by Vladyslav Mi on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
